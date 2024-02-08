ed11872e68fd874c827132ff0c887050bd407caa
Sachintha Rashan <91013948+SACHIBOT@users.noreply.github.com>
2024-02-08 15:58:59 +0530

    fsv

---
+++ imgs/image/imag2/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/libs/tmp.js
@@ -0,0 +1,9 @@
+const isUrl2 = (url) => {
+	return url.match(
+		new RegExp(
+			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&/=]*)/,
+			'gi'
+		)
+	)
+}
+module.exports = {  isUrl2 }
