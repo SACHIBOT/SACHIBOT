8d115f573dd639966b3d2aab45b1152949cf5995
Sachintha Rashan <91013948+SACHIBOT@users.noreply.github.com>
2024-02-12 17:52:40 +0530

    Delete imgs/image/imag2/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/lib/tmp.js

--- imgs/image/imag2/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/lib/tmp.js
+++
@@ -1,9 +0,0 @@
-const isUrl2 = (url) => {
-	return url.match(
-		new RegExp(
-			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&/=]*)/,
-			'gi'
-		)
-	)
-}
-module.exports = {  isUrl2 }
