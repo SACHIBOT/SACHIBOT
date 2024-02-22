611ac3dcd2cee41c26eb7bf3f8426bfe36d6266f
Sachintha Rashan <91013948+SACHIBOT@users.noreply.github.com>
2024-02-22 21:17:06 +0530

    Update config.js

--- imgs/image/imag2/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/index/config.js
+++ imgs/image/imag2/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/index/config.js
@@ -4,6 +4,8 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+const BOTNAME = 'MOVIE-MYSTIC';
+const FOOTERNAME = '‌ꜱᴀᴄʜɪʙᴏᴛ';
 module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 BOTNAME : 'MOVIE-MYSTIC',
