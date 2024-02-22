530a4c755dc86ff8882b633e0796a1663faaae0a
Sachintha Rashan <91013948+SACHIBOT@users.noreply.github.com>
2024-02-22 19:37:32 +0530

    Update config.js

--- imgs/image/imag2/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/index/config.js
+++ imgs/image/imag2/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/index/config.js
@@ -12,6 +12,7 @@ ALWAYS_ONLINE: process.env.ALWAYS_ONLINE === undefined ? 'false' : process.env.A
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '500': process.env.MAX_SIZE,/*add this in megabytes*/
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'true' : process.env.ONLY_ME,
+VERSION: process.env.VERSION === undefined ? 'true' : process.env.VERSION,
 ALIVE: process.env.ALIVE === undefined ? `🍿 *Welcome to ${BOTNAME} !* 🍿\n\n🎬 _Get ready for an incredible movie experience!_ 🎬\n\n📥 _Simply send me the name of the movie you want to download, and I'll provide you with direct download links._\n\n🌟 _No more hassle, just pure entertainment at your fingertips!_ 🌟\n\n🔥 _Let's dive into the world of movies together!_ 🔥\n\n${FOOTERNAME}` : process.env.ALIVE,
 LOGO: `https://telegra.ph/file/800b484e6723535f201b1.jpg`,
 DEVNUMBER: '94725881990',
